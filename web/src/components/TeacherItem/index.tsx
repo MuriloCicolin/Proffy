c81c7fee5be9dcac172e15b7445ff861fc461434
MuriloCicolin <mu_cicolin@hotmail.com>
2020-08-04 23:07:13 -0300

    first commit

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import whatsappIcon from "../../assets/icons/whatsapp.svg";
+
+import "./styles.css";
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/49662901?s=460&u=e52214f21bdd3fde655087aaff2bdc55649b29d3&v=4"
+          alt="Murilo Cicolin"
+        />
+        <div>
+          <strong>Murilo Cicolin</strong>
+          <span>Quimica</span>
+        </div>
+      </header>
+      <p>
+        Aulas de Matemática bem estruturadas e expositiva.
+        <br /> <br />
+        Estude para o ENEM e Vestibulares no Melhor Cursinho com o Melhor Preço.
+        Pague em até 12x. ENEM com a melhor preparação. Parcelas por menos de R$
+        15 reais.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 15,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em Contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
